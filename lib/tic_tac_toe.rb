c8929f8bd7c8ba71c5561f4165aa6445895d31a6
Ethel C <cofresi.ethel@gmail.com>
2018-03-21 01:59:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,8 +39,8 @@ def position_taken?(index)
   !(@board[index].nil? || @board[index] == " ")
 end
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(@board, index)
 end
 
 def turn_count(board)
